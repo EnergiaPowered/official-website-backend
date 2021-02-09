5d5c18f843b6b0ac54071a05246e3f20186b8b97
Waleed Hesham <waleedhesham2001@gmail.com>
2021-02-09 18:28:08 +0200

    Edited unverified function

--- bin/unverified.js
+++ bin/unverified.js
@@ -1,14 +1,17 @@
 const { User } = require('../models/User');
 
-module.exports =  function(){ 
-	User.find({ verified: false }).toArray(function(err, user) {
-		if (err) throw err;
+module.exports = function() {
+    User.find({ verified: false } , (err, users) => {
+        if (err) throw err;
 
-		let currentDate = new Date().getTime();
-		if(parseInt(currentDate - user.createdAt) >= (24*60*60*1000)){
-			User.deleteOne({ _id: user._id }, function(err, obj) {
-				if (err) throw err;
-			});
-		}
-	});
+        let currentDate = new Date().getTime();
+
+        users.map(user => {
+			if(parseInt(currentDate - user.addTimeStamp) >= (24*60*60*1000)){
+				User.deleteOne({ _id: user._id }, function(err, obj) {
+					if (err) throw err;
+				});
+			} 
+        })
+    })
 }
