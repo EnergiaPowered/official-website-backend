fa717b2321c11fc672fda0751655c9dc152abbe6
Mohamed Emad <bon.dok2000@hotmail.com>
2021-11-15 18:59:57 +0200

    Added timestamp to form

--- models/Form.js
+++ models/Form.js
@@ -1,30 +1,33 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const formSchema = new Schema({
-  formSubmitted: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  /* when the deadline of the form is over */
-  formFinished: {
-    type: String,
-    required: true,
-  },
-  questions: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Question",
+const formSchema = new Schema(
+  {
+    formSubmitted: {
+      type: String,
+      required: true,
     },
-  ],
-  endDate: {
-    type: Date,
-    required: true,
+    title: {
+      type: String,
+      required: true,
+    },
+    /* when the deadline of the form is over */
+    formFinished: {
+      type: String,
+      required: true,
+    },
+    questions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Question",
+      },
+    ],
+    endDate: {
+      type: Date,
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Form", formSchema);
