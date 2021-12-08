3a8b21288d2130411f327013cb6a9305899137e4
Mohamed Emad <bon.dok2000@hotmail.com>
2021-12-08 11:10:40 +0200

    Set title to be unique

--- models/Form.js
+++ models/Form.js
@@ -5,6 +5,7 @@ const formSchema = new Schema(
     title: {
       type: String,
       required: true,
+      unique: true,
     },
     description: {
       type: String,
@@ -35,7 +36,7 @@ const formSchema = new Schema(
           default: "Text",
         },
         placeholder: { type: String },
-        isReq: {
+        isRequired: {
           type: Boolean,
           required: true,
         },
