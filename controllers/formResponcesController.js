d9bab7aab845dbbd1f9a54147353f52eb57cbaca
Mohamed Emad <bon.dok2000@hotmail.com>
2022-03-11 18:27:52 +0200

    Added get all forms and specific one and specific one by id

--- controllers/formResponcesController.js
+++ controllers/formResponcesController.js
@@ -13,13 +13,46 @@ module.exports = {
   },
   getAllFormResponce: async (req, res) => {
     try {
-      const docs = [];
-      const getresponces = await formResponce.get();
-      //We have to loop over the collection to get all documents
-      getresponces.forEach((doc) => {
-        docs.push(doc);
-      });
-      res.status(200).json(docs);
+      if (req.query.form) {
+        const docs = [];
+        const documentsOfForm = await formResponce
+          .where("form", "==", req.query.form)
+          .get();
+        documentsOfForm.forEach((doc) => {
+          docs.push(doc);
+        });
+        res.status(200).json(docs);
+      } else {
+        const docs = [];
+        const getresponces = await formResponce.get();
+        //We have to loop over the collection to get all documents
+        getresponces.forEach((doc) => {
+          docs.push(doc);
+        });
+        res.status(200).json(docs);
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  },
+  getDocumentById: async (req, res) => {
+    try {
+      const responce = await formResponce.doc(req.params.id).get();
+      if (!responce.form) {
+        return res.status(404).json({ message: "notfound" });
+      }
+      res.status(200).json(responce);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  },
+  getDocumentByForm: async (req, res) => {
+    try {
+      console.log("hi");
+      const documentsOfForm = await formResponce
+        .where("form", "==", req.query.form)
+        .get();
+      res.status(200).send(documentsOfForm);
     } catch (err) {
       res.status(500).send(err);
     }
--- routes/form_res.js
+++ routes/form_res.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const formResponcesController = require("../controllers/formResponcesController");
 const router = express.Router();
 const formResponceController = require("../controllers/formResponcesController");
 router.post("/formRes", formResponceController.saveFormResponce);
 router.get("/formRes", formResponceController.getAllFormResponce);
+router.get("/formRes/:id", formResponceController.getDocumentById);
 module.exports = router;
