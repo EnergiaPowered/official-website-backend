1212d7ae1ebfdd7f0e80b201e5b9085446a57da9
abdullahalshawafi <abdullahadel.aam@gmail.com>
2022-05-04 03:27:25 +0200

    Added gates page and code splitting

---
+++ client/src/modules/Gates/services/gates.services.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: "1st Gate: Build",
+    desc: "In this gate, you will know how to build your CV to meet company's requirements.",
+  },
+  {
+    title: "2nd Gate: Learn",
+    desc: "Here, you will learn more about our partner companies and get familiar with the work processes, systems and technologies there.\nYou can enter these gates when you join our online sessions on Friday 13/5, and Saturday 14/5.",
+  },
+  {
+    title: "3rd Gate: Practice",
+    desc: "Open this gate to get into the practical life through a special experience with senior engineers in real life sites.\nEnter this gate by attending our Field Trips from Sunday 15/5 to Thursday 19/5.",
+  },
+];
