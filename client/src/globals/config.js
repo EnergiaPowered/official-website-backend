c75fabdedd879337d05275da9b3c7e60b4c82005
AliKhalaf1 <47224980+AliKhalaf1@users.noreply.github.com>
2022-11-11 19:17:35 +0200

    Update config.js

--- client/src/globals/config.js
+++ client/src/globals/config.js
@@ -2,11 +2,11 @@ const configs = {
   API_BASE_URL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000/api/"
-      : "https://energia21.herokuapp.com/api/",
+      : "https://ep-api.onrender.com/api/",
   HOST:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000/"
-      : "https://energia21.herokuapp.com/",
+      : "https://ep-api.onrender.com/",
 };
 
 export default configs;
