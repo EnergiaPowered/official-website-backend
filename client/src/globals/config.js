c659523659444fcd6399a9790ab62a094d9087e7
AliKhalaf1 <47224980+AliKhalaf1@users.noreply.github.com>
2022-11-27 20:41:38 +0200

    Update config.js

--- client/src/globals/config.js
+++ client/src/globals/config.js
@@ -2,11 +2,11 @@ const configs = {
   API_BASE_URL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000/api/"
-      : "https://ep-api.onrender.com/api/",
+      : "https://handsome-worm-woolens.cyclic.app",
   HOST:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000/"
-      : "https://ep-api.onrender.com/",
+      : "https://handsome-worm-woolens.cyclic.app",
 };
 
 export default configs;
